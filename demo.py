6059e2a3ccbbbb81e3df5eebcc90d3b684f2278d
prabhu408 <prabhakarmalipatil@gmail.com>
2022-06-28 15:45:26 +0530

    MySql connection

--- demo.py
+++ demo.py
@@ -1,1 +1,13 @@
 print("Hello!")
+import mysql.connector
+
+mydb = mysql.connector.connect(host="localhost", user="root", password="1234", database="patils")
+
+mycorsor = mydb.cursor()
+
+mycorsor.execute("select * from student")
+
+result = mycorsor.fetchall()
+
+for i in result:
+    print(i)
